5382a812ba8ffcab4d4481989cb1c36073a6544a
Anne Edmondson <edmondson.anne@gmail.com>
2019-04-02 01:55:33 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,10 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :color, :size, :material
+  
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def cobble(condition = "new")
+    puts ""
+end
